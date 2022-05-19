84760fa0efb06fed221a50f45bd675f1999816c1
SapitoBot <103408838+AgromosSpboy@users.noreply.github.com>
2022-05-19 09:51:09 -0400

    Update donate.js

--- plugins/donate.js
+++ plugins/donate.js
@@ -4,11 +4,10 @@ let handler =  m => m.reply(`
 puedes donar si quieres ayudar a mantener el bot activo
 
 ▢ *PayPal*
-• *Link :* https://paypal.me/fg98f
-▢ *Mercado Pago Arg*
-• *Link :* https://mpago.la/1F3r6JH
-▢ *Tigo Money*
-• *N° :*  75140648 
+• *Link :* 
+
+
+• 
 
 _Al donar consigues_  *Premium* 
 
