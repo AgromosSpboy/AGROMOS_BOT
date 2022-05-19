48af1c13f070d3ab42c6bd3ad146fe0479bab5a1
SapitoBot <103408838+AgromosSpboy@users.noreply.github.com>
2022-05-19 09:49:47 -0400

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -4,30 +4,28 @@ function handler(m) {
   
   m.reply(`
 *≡ OWNER*
-  *FG98*
-▢ Instagram :
-  • https://instagram.com/fg98._
-▢ WhatsApp :
-  • (escribe solo si es necesario) 
-▢ GitHub :
-  • https://github.com/FG98F
-▢ Telegram : 
-  • t.me/fgsupp_bot (FG) 
-  • t.me/fg98ff (canal)
-  • t.me/fgawgp (grupo)
-▢ Facebook : 
-  • https://facebook.com/fg98ff/
-  • https://facebook.com/fg98A/
-▢ YouTube : 
+  *Agromos*
+
+  
+
+  
+  
+  
+  
+  
+
+  
+  
+▢ YouTube : https://youtube.com/channel/UCYfOaeRkOwMXzLxx11nwecA
   • https://youtube.com/fg98f
   
  *≡ HELPERS*
-  *CRISS*
-▢ Instagram : https://www.instagram.com/sayurizuniga3
-▢ WhatsApp : wa.me/
-  *ANDREA*
-▢ Instagram : https://
-▢ WhatsApp : wa.me/573125484672
+  *Creador*
+▢ Youtube https://youtube.com/channel/UCYfOaeRkOwMXzLxx11nwecA
+▢ WhatsApp : wa.me/523931806576
+  *Bot oficial*
+▢ YouTube https://youtube.com/channel/UCYfOaeRkOwMXzLxx11nwecA
+▢ WhatsApp : wa.me/595983186566
 `) 
 
 
