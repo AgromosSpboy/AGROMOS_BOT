cbb93b577cc05e28c3abdb5eec6d8fd787eddb76
SapitoBot <103408838+AgromosSpboy@users.noreply.github.com>
2022-05-19 14:09:55 -0400

    Create estado.js

---
+++ plugins/estado.js
@@ -0,0 +1,35 @@
+let handler = async (m, { conn, command, usedPrefix }) => {
+
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+
+let mentionedJid = [who]
+
+conn.send3Button(m.chat, `
+
+*ミ💚 Hola estimado usuario 💚彡*
+
+*ミ😈 Estado del bot 😈彡*
+
+*=> Bot activo para tus servicios😈🔥*
+
+*=> Bot  para cualquier uso😹💚*
+
+`.trim(), 'AGROMOS SP - Bot', '🔵 MENÚ 🔵', `#menu`, '🔴 MENÚ NUEVO🔴', `#menunuevo`, '🟡 MENÚ AUDIOS 🟡', `#menuaudios`, m, false, { contextInfo: { mentionedJid }})}
+
+handler.command = /^(estado|status|estate|state|stado|stats)$/i
+
+module.exports = handler
+
+© 2022 GitHub, Inc.
+
+Terms
+
+Privacy
+
+Security
+
+Status
+
+Docs
+
+Contact GitHub
